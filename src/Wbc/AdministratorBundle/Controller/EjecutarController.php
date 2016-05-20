9c5e2f86b36073f4457c4f6e82bfc994b7ecec3b
Rene Estrada <restrada@apparkitekt.com>
2016-05-20 00:20:06 -0600

    add ejecutar entity

---
+++ src/Wbc/AdministratorBundle/Controller/EjecutarController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Wbc\AdministratorBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Wbc\AdministratorBundle\Entity\Ejecutar;
+use Wbc\AdministratorBundle\Form\EjecutarType;
+
+/**
+ * Ejecutar controller.
+ *
+ */
+class EjecutarController extends Controller
+{
+    /**
+     * Lists all Ejecutar entities.
+     *
+     */
+    public function indexAction()
+    {
+        $this->get('Services')->setMenuItem('Ejecutar');
+        $em = $this->getDoctrine()->getManager();
+
+        $ejecutars = $em->getRepository('WbcAdministratorBundle:Ejecutar')->findAll();
+
+        return $this->render('ejecutar/index.html.twig', array(
+            'ejecutars' => $ejecutars,
+        ));
+    }
+
+    /**
+     * Creates a new Ejecutar entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $this->get('Services')->setMenuItem('Ejecutar');
+
+        $ejecutar = new Ejecutar();
+        $form = $this->createForm('Wbc\AdministratorBundle\Form\EjecutarType', $ejecutar);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {            
+            
+            $now = new \DateTime('now');            
+            $ejecutar->setCreacion($now);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ejecutar);
+            $em->flush();
+
+            $this->get('Services')->addFlash('success', $this->get('translator')->trans('Ejecutar created!'));
+
+            return $this->redirectToRoute('ejecutar_index');
+        }
+
+        return $this->render('ejecutar/new.html.twig', array(
+            'ejecutar' => $ejecutar,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Ejecutar entity.
+     *
+     */
+    public function showAction(Ejecutar $ejecutar)
+    {
+
+        $this->get('Services')->setMenuItem('Ejecutar');
+        $changes = $this->get('Services')->getLogsByEntity($ejecutar);
+
+        return $this->render('ejecutar/show.html.twig', array(
+            'ejecutar' => $ejecutar,
+            'changes' => $changes,
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Ejecutar entity.
+     *
+     */
+    public function editAction(Request $request, Ejecutar $ejecutar)
+    {
+        $this->get('Services')->setMenuItem('Ejecutar');
+        $editForm = $this->createForm('Wbc\AdministratorBundle\Form\EjecutarType', $ejecutar);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ejecutar);
+            $em->flush();
+
+            $this->get('Services')->addFlash('success', $this->get('translator')->trans('Ejecutar updated!'));
+
+            return $this->redirectToRoute('ejecutar_index');
+        }
+
+        return $this->render('ejecutar/edit.html.twig', array(
+            'ejecutar' => $ejecutar,
+            'form'        => $editForm->createView()
+        ));
+    }
+
+    /**
+     * Deletes a Ejecutar entity.
+     *
+     */
+    public function deleteAction(Request $request, Ejecutar $ejecutar)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($ejecutar);
+        $em->flush();
+
+        $this->get('Services')->setFlash('danger', $this->get('translator')->trans('Ejecutar deleted!'));
+
+        return $this->redirectToRoute('ejecutar_index');
+    }
+
+}
---
+++ src/Wbc/AdministratorBundle/Form/EjecutarType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wbc\AdministratorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EjecutarType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+//            ->add('nombreProceso', null, ['attr' => ['class' => 'form-control']])
+            ->add('tiempo', null, ['attr' => ['class' => 'form-control']])
+            ->add('memoria', null, ['attr' => ['class' => 'form-control']])
+            ->add('terminado', null, ['attr' => ['class' => '']])
+//            ->add('creacion', DateTimeType::class, ['format' => 'yyyy-MM-dd HH:mm', 'widget' => 'single_text', 'attr' => ['class' => 'datetimepicker form-control']])
+            ->add('configuracion', null, ['attr' => ['class' => 'select2 form-control']])
+            ->add('palabra_reservada', null, ['attr' => ['class' => 'select2 form-control']])
+            ->add('user', null, ['attr' => ['class' => 'select2 form-control']])
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Wbc\AdministratorBundle\Entity\Ejecutar'
+        ));
+    }
+}
