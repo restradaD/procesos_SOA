192d09171234742b1f54d45d38368175e836cd99
Rene Estrada <restrada@apparkitekt.com>
2016-05-19 23:41:51 -0600

    add configuracion crud

---
+++ src/Wbc/AdministratorBundle/Controller/ConfiguracionController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Wbc\AdministratorBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Wbc\AdministratorBundle\Entity\Configuracion;
+use Wbc\AdministratorBundle\Form\ConfiguracionType;
+
+/**
+ * Configuracion controller.
+ *
+ */
+class ConfiguracionController extends Controller
+{
+    /**
+     * Lists all Configuracion entities.
+     *
+     */
+    public function indexAction()
+    {
+        $this->get('Services')->setMenuItem('Configuracion');
+        $em = $this->getDoctrine()->getManager();
+
+        $configuracions = $em->getRepository('WbcAdministratorBundle:Configuracion')->findAll();
+
+        return $this->render('configuracion/index.html.twig', array(
+            'configuracions' => $configuracions,
+        ));
+    }
+
+    /**
+     * Creates a new Configuracion entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $this->get('Services')->setMenuItem('Configuracion');
+
+        $configuracion = new Configuracion();
+        $form = $this->createForm('Wbc\AdministratorBundle\Form\ConfiguracionType', $configuracion);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $now = new \DateTime('now');            
+            $configuracion->setCreacion($now);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($configuracion);
+            $em->flush();
+
+            $this->get('Services')->addFlash('success', $this->get('translator')->trans('Configuracion created!'));
+
+            return $this->redirectToRoute('configuracion_index');
+        }
+
+        return $this->render('configuracion/new.html.twig', array(
+            'configuracion' => $configuracion,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Configuracion entity.
+     *
+     */
+    public function showAction(Configuracion $configuracion)
+    {
+
+        $this->get('Services')->setMenuItem('Configuracion');
+        $changes = $this->get('Services')->getLogsByEntity($configuracion);
+
+        return $this->render('configuracion/show.html.twig', array(
+            'configuracion' => $configuracion,
+            'changes' => $changes,
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Configuracion entity.
+     *
+     */
+    public function editAction(Request $request, Configuracion $configuracion)
+    {
+        $this->get('Services')->setMenuItem('Configuracion');
+        $editForm = $this->createForm('Wbc\AdministratorBundle\Form\ConfiguracionType', $configuracion);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($configuracion);
+            $em->flush();
+
+            $this->get('Services')->addFlash('success', $this->get('translator')->trans('Configuracion updated!'));
+
+            return $this->redirectToRoute('configuracion_index');
+        }
+
+        return $this->render('configuracion/edit.html.twig', array(
+            'configuracion' => $configuracion,
+            'form'        => $editForm->createView()
+        ));
+    }
+
+    /**
+     * Deletes a Configuracion entity.
+     *
+     */
+    public function deleteAction(Request $request, Configuracion $configuracion)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($configuracion);
+        $em->flush();
+
+        $this->get('Services')->setFlash('danger', $this->get('translator')->trans('Configuracion deleted!'));
+
+        return $this->redirectToRoute('configuracion_index');
+    }
+
+}
---
+++ src/Wbc/AdministratorBundle/Form/ConfiguracionType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wbc\AdministratorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConfiguracionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('proceso', null, ['attr' => ['class' => 'form-control']])
+            ->add('memoria', null, ['attr' => ['class' => 'form-control']])
+            ->add('cantidadBloque', null, ['attr' => ['class' => 'form-control']])
+            ->add('activo', null, ['attr' => ['class' => '']])
+//            ->add('creacion', DateTimeType::class, ['format' => 'yyyy-MM-dd HH:mm', 'widget' => 'single_text', 'attr' => ['class' => 'datetimepicker form-control']])
+//            ->add('fechaExpira', DateTimeType::class, ['format' => 'yyyy-MM-dd HH:mm', 'widget' => 'single_text', 'attr' => ['class' => 'datetimepicker form-control']])
+            ->add('expira', null, ['attr' => ['class' => 'form-control']])
+            ->add('politica', null, ['attr' => ['class' => 'select2 form-control']])
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Wbc\AdministratorBundle\Entity\Configuracion'
+        ));
+    }
+}
